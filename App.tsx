fea478abf43052eab3aca9d0872a555892c398e0
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2024-09-13 07:45:50 +0700

    Increase Array by one

--- App.tsx
+++ App.tsx
@@ -4,27 +4,30 @@ import { View, Text, Button } from "react-native";
 // const randomDiceRoll = () => Math.floor(Math.random() * 6) + 1;
 
 export default function App() {
-  // const [diceRolls, setDiceRolls] = useState<number[]>([]);
-  const [count, setCount] = useState<number>(0);
+  const [arrNumbers, setArrNumbers] = useState<number[]>([1, 2, 3]);
 
   return (
     <View>
       <Button
-        title="Increase Count"
+        title="Increase Array one"
         onPress={() => {
-          setCount(count + 1);
+          let newArr = [...arrNumbers].map((x) => x + 1);
+          setArrNumbers(newArr);
         }}
       />
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          textAlign: "center",
-          margin: 20,
-        }}
-      >
-        {count}
-      </Text>
+      {arrNumbers.map((item, index) => (
+        <Text
+          key={index}
+          style={{
+            fontSize: 24,
+            fontWeight: "bold",
+            textAlign: "center",
+            margin: 20,
+          }}
+        >
+          {item}
+        </Text>
+      ))}
     </View>
   );
 }
